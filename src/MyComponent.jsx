2cdfa868a3afe58cfcc37e38d28b54c9a7b07a26
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2025-01-12 17:11:29 +0600

    Subtract button

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -5,16 +5,21 @@ function MyComponent() {
     const [count, setCount] = useState(0);
 
     useEffect(() => {
-        document.title = `My counter program`
-    }, [])
+        document.title = `Count: ${count}`;
+    }, [count])
 
     function addCount() {
         setCount(c => c + 1);
     }
 
+    function subtractCount() {
+        setCount(c => c - 1);
+    }
+
     return(<>
         <p>Count: {count}</p>
         <button onClick={addCount}>Add</button>
+        <button onClick={subtractCount}>Subtract</button>
     </>)
 }
 export default MyComponent

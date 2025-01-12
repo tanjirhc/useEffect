789a35bc80607f3ad1f9e8564db6cab0d9237efe
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2025-01-12 17:18:07 +0600

    color change button

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react'
 function MyComponent() {
 
     const [count, setCount] = useState(0);
+    const [color, setColor] = useState('red');
 
     useEffect(() => {
         document.title = `Count: ${count}`;
@@ -16,10 +17,15 @@ function MyComponent() {
         setCount(c => c - 1);
     }
 
+    function changeColor() {
+        setColor(c => c === 'red' ? 'blue' : 'red');
+    }
+
     return(<>
-        <p>Count: {count}</p>
+        <p style={{color: color}}>Count: {count}</p>
         <button onClick={addCount}>Add</button>
-        <button onClick={subtractCount}>Subtract</button>
+        <button onClick={subtractCount}>Subtract</button><br/>
+        <button onClick={changeColor}>Change Color</button>
     </>)
 }
 export default MyComponent
